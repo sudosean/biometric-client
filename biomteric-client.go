f9fe98a1e7a5a0c6ddfa9d6eedde3308311d0d5e
sean <sean.large1@gmail.com>
2020-01-15 21:28:27 -0500

    initial commit

---
+++ biomteric-client.go
@@ -0,0 +1,17 @@
+package biometric 
+
+import(
+	"fmt"
+)
+
+func CreateTemplate()  {
+	fmt.Println("Template Created")
+}
+
+func CompareList(){
+	fmt.Println("List Compared")
+}
+
+func AlgoInfo(){
+	fmt.Println("Algo Inforation")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sudosean/biometric-client
+
+go 1.13
